dabe9988df97d164379af6ab60f780fc0e3b8ffb
Hugh Handcox <82186765+hhandcox@users.noreply.github.com>
2023-01-06 23:43:37 -0600

    Video and Card Section Added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -10,8 +10,9 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
+        <Home />
         <Routes>
-          <Route path='/' exact component ={Home}/>
+          <Route path='/' exact component={Home}/>
         </Routes>
       </Router>
     </div>
