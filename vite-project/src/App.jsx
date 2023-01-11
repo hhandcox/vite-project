95a217f24c84933bc53a2fb5f1a3b2032e633ccc
Hugh Handcox <82186765+hhandcox@users.noreply.github.com>
2023-01-10 19:36:14 -0600

    CSS and Pages Changes - Routes/Pages Still Need Work

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/pages/Home';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -16,7 +17,9 @@ function App() {
               <Route path='/services' component={<Services />}/>
               <Route path='/products' component={<Products />}/>
               <Route path='/sign-up' component={<SignUp />}/>
+              <Route path='*' component={<Home />}/>
         </Routes>
+        <Footer />
       </>
   )
 }
--- vite-project/src/components/pages/Services.jsx
+++ vite-project/src/components/pages/Services.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import '../../App.css';
+import Cards from '../Cards';
 
-export default function Services(){
-    return <h1 className='services'>Services</h1>
+function Services(){
+    return (
+        <>
+            <h1 className='services'>Services</h1>
+            <Cards />
+        </>
+    )
 }
+
+export default Services;
