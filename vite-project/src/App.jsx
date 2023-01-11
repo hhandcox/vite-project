929bc775b8e421671541101ac4d16189f292ef73
Hugh Handcox <82186765+hhandcox@users.noreply.github.com>
2023-01-10 18:54:11 -0600

    Fixed BrowserRouter

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,17 +8,16 @@ import SignUp from './components/pages/SignUp';
 
 
 function App() {
-
   return (
-    <BrowserRouter>
+      <>
         <Navbar />
         <Routes>
-            <Route path='/' exact element={Home}/>
-              <Route path='/services' component={Services}/>
-              <Route path='/products' component={Products}/>
-              <Route path='/sign-up' component={SignUp}/>
+            <Route path='/' exact element={<Home />}/>
+              <Route path='/services' component={<Services />}/>
+              <Route path='/products' component={<Products />}/>
+              <Route path='/sign-up' component={<SignUp />}/>
         </Routes>
-    </BrowserRouter>
+      </>
   )
 }
 
